c72dcd996dbcd78bbd72d926695783878faaa53f
abhishek <abhishek.labla@ajatus.co.in>
2024-07-08 18:34:20 +0530

    dgv

---
+++ src/data/data.js
@@ -0,0 +1,85 @@
+import Dosa from "../images/dosa.jpg";
+import Chola from "../images/chhola.jpg";
+import Idli from "../images/idli.jpg";
+import MasalaDosa from "../images/masala.jpg";
+import Paneer from "../images/paneer.jpg";
+import VegThali from "../images/vegThali.jpg";
+import chickenThali from '../images/chickenthali.webp';
+import paneerthali from '../images/paneerthali.webp';
+import Mashroom from '../images/mashroom.jpeg';
+import mutton from '../images/Mutton.jpg';
+
+
+export const MenuList = [
+  {
+    name: "Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Dosa,
+    price: 80,
+  },
+  {
+    name: "Chole Bhature",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Chola,
+    price: 150,
+  },
+  {
+    name: "Idli Sambhar",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Idli,
+    price: 120,
+  },
+  {
+    name: "Masala Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: MasalaDosa,
+    price: 100,
+  },
+  {
+    name: "Paneer Butter Masala",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Paneer,
+    price: 200,
+  },
+  {
+    name: "Veg Thali",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: VegThali,
+    price: 120,
+  },
+  {
+    name: "Chicken Thali",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: chickenThali,
+    price: 200,
+  },
+  {
+    name: "Paneer Thali",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: paneerthali,
+    price: 180,
+  },
+  {
+    name: "Mashroom Thali",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Mashroom,
+    price: 180,
+  },
+  {
+    name: "Mutton Thali",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: mutton,
+    price: 300,
+  },
+ 
+];
